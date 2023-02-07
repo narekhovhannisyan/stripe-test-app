b5d8810e04be4778a99641be3a899b2314c7527b
Narek Hovhannisyan <narekgilmour@gmail.com>
2023-02-07 20:20:27 +0400

    config: init stripe vars.

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,20 @@
+type PaymentStatuses = {
+  [key: string]: number
+}
+
 export default {
   MODE: process.env.MODE || 'development',
   PORT: process.env.PORT && parseInt(process.env.PORT) || 3010,
   REQUIRED_VARIABLES: [],
-  RETRY_AFTER_HEADER: process.env.RETRY_AFTER_HEADER || 2 * 60 * 60
+  RETRY_AFTER_HEADER: process.env.RETRY_AFTER_HEADER || 2 * 60 * 60,
+  STRIPE: {
+    PUBLISHABLE_KEY: process.env.STRIPE_PUBLISHABLE_KEY || '',
+    SECRET_KEY: process.env.STRIPE_SECRET_KEY || '',
+    WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET || '',
+    API_VERSION: '2022-11-15' as const
+  },
+  SERVICES: {
+    STRIPE: 'Stripe',
+    SEQUELIZE: 'Sequelize'
+  }
 }
