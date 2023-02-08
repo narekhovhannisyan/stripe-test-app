f5a593960378243deffb723317ece5aa876f8106
Narek Hovhannisyan <narekgilmour@gmail.com>
2023-02-08 22:57:08 +0400

    middleware: re-export validations from index.

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,3 +1,4 @@
 export * as AuthorizationMiddleware from './authorization.middleware'
 export * as CommonMiddleware from './common.middleware'
 export * from './error-handler.middleware'
+export * from './validations'
