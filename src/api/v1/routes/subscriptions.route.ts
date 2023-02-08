80193128cf5f169a2d1b095451c518bd62442f96
Narek Hovhannisyan <narekgilmour@gmail.com>
2023-02-08 22:58:00 +0400

    routes: use subscriptions validation.

--- src/api/v1/routes/subscriptions.route.ts
+++ src/api/v1/routes/subscriptions.route.ts
@@ -2,9 +2,12 @@ import { Router } from 'express'
 
 import * as SubscriptionsService from '../services/subscriptions.service'
 
+import { SubscriptionsValidationMiddleware } from '../../../middleware'
+
 const router = Router()
 
 router.get('/',
+  SubscriptionsValidationMiddleware.validateGetUserArgs,
   SubscriptionsService.getSubscriptionByEmail
 )
 
