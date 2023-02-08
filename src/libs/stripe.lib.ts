21343400894d76bca2318e0f745697477aacc3d4
Narek Hovhannisyan <narekgilmour@gmail.com>
2023-02-08 22:39:29 +0400

    libs: implement stripe invoice payment.

--- src/libs/stripe.lib.ts
+++ src/libs/stripe.lib.ts
@@ -1,3 +1,4 @@
+import Bluebird from 'bluebird'
 import Stripe from 'stripe'
 
 import { ConnectionLogger } from '../utils/connection-logger.util'
@@ -45,3 +46,45 @@ export const constructEvent = (params: ConstructEventParams) => {
  * Get customer information based on `id`.
  */
 export const getCustomerData = (id: string) => stripe.customers.retrieve(id)
+
+/**
+ * Creates subscription for given `customer`.
+ */
+export const createSubscription = (customerId: string, items: any[]) => stripe.subscriptions.create({ customer: customerId, items })
+
+/**
+ * _____________________ TASK 2 _____________________
+ */
+
+/**
+ * Gets invoices between `dateStart` and `dateEnd`.
+ */
+const getInvoices = (dateStart: string, dateEnd: string) => {
+  const milliStart = new Date(dateStart)
+  const milliEnd = new Date(dateEnd)
+  const query = `created>${milliStart.getDate()} AND created<${milliEnd.getDate()}`
+
+  return stripe.invoices.search({ query })
+}
+
+/**
+ * Pays invoice with given id.
+ */
+const payInvoice = (id: string) => stripe.invoices.pay(id)
+
+/**
+ * Pays `invoices` from given array.
+ */
+const payInvoices = (invoices: string[]) => Bluebird.map(invoices, payInvoice)
+
+/**
+ * Pays invoices from `dateStart` to `dateEnd`.
+ */
+const payInvoicesFrom = (dateStart: string, dateEnd: string) => getInvoices(dateStart, dateEnd)
+  .then((response) => {
+    const invoiceIds = response.data.map((invoice) => invoice.id)
+
+    return payInvoices(invoiceIds)
+  })
+
+// payInvoicesFrom('2019-01-01', '2022-01-01')
