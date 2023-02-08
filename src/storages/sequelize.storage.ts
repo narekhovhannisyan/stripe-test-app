350addc84295e8e8c815476b10e587b4926bb0f6
Narek Hovhannisyan <narekgilmour@gmail.com>
2023-02-08 10:39:09 +0400

    storages: init sequelize connection.

---
+++ src/storages/sequelize.storage.ts
@@ -0,0 +1,33 @@
+import { Options, Sequelize } from 'sequelize'
+
+import CONFIG from '../config'
+
+import { ConnectionLogger } from '../utils/connection-logger.util'
+
+const { DATABASE_URL } = CONFIG
+
+const SERVICE_NAME = 'Sequelize'
+const logger = ConnectionLogger(SERVICE_NAME)
+
+/**
+ * Options for new Sequelize instance.
+ */
+const OPTIONS: Options = {
+  logging: false,
+  dialect: 'postgres',
+  dialectOptions: {
+    useUTC: false,
+    multipleStatements: true,
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
+}
+
+export const SequelizeClient = new Sequelize(DATABASE_URL, OPTIONS)
+
+SequelizeClient.sync()
+SequelizeClient.authenticate(OPTIONS)
+  .then(logger.logConnectionSuccess)
+  .catch(logger.logConnectionFailure)
